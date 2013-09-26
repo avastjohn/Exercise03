f6fbafcf5158f7c8719eb9518f84d07fe9897a20
Student <user@chromebox-001.(none)>
2013-09-25 17:13:04 -0700

    Add user error check

--- calculator.py
+++ calculator.py
@@ -1,6 +1,14 @@
 #import functions from arthimetic file
 from arithmetic import *
 
+# error checking
+def check_errors(args, num):
+    if len(args) != num:
+        print "Your input sucks. Try again."
+        return False
+
+    return True
+
 #get calculator input from user
 user_input = raw_input("> ")
 
@@ -15,24 +23,32 @@ while user_input != "q":
 
     # turn all numbers into integers
     numbers = map(int, numbers)
-    
+
     #uses operator to determine which function to call
     if operator == "+":
-        print add(numbers[0],numbers[1])
+        if check_errors(numbers, 2):
+            print add(numbers[0],numbers[1])
     elif operator == "-":
-        print subtract(numbers[0],numbers[1])
+        if check_errors(numbers, 2):
+            print subtract(numbers[0],numbers[1])
     elif operator == "*":
-        print multiply(numbers[0],numbers[1])    
+        if check_errors(numbers, 2):
+            print multiply(numbers[0],numbers[1])    
     elif operator == "/":
-        print divide(numbers[0],numbers[1])
+        if check_errors(numbers, 2):
+            print divide(numbers[0],numbers[1])
     elif operator == "square":
-        print square(numbers[0])
+        if check_errors(numbers, 1):
+            print square(numbers[0])
     elif operator == "cube":
-        print cube(numbers[0])
+        if check_errors(numbers, 1):
+            print cube(numbers[0])
     elif operator == "pow":
-        print power(numbers[0],numbers[1])
+        if check_errors(numbers, 2):
+            print power(numbers[0],numbers[1])
     elif operator == "mod":
-        print mod(numbers[0],numbers[1])        
+        if check_errors(numbers, 2):
+            print mod(numbers[0],numbers[1])        
     else:
         print "Not a valid calculator command. Please try again!"
 
